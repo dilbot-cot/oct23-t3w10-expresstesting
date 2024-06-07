15dfa99842e415f4e311b882654f2c0fb60d5383
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-06-08 09:33:33 +1000

    feat: server hello world

---
+++ src/server.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.json({
+        message:"Hello World!"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
